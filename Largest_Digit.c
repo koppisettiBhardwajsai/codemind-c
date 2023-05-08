ad51c92859dc8018d098eb4ef396464967bb9113
koppisettiBhardwajsai <120609887+koppisettiBhardwajsai@users.noreply.github.com>
2023-05-08 21:18:14 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int l=0;     while(n>0)     {         int r=n%10;         if(r>l)         {             l=r;         }         n=n/10;     }     printf("%d",l); }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int l=0;
+    while(n>0)
+    {
+        int r=n%10;
+        if(r>l)
+        {
+            l=r;
+        }
+        n=n/10;
+    }
+    printf("%d",l);
+}
