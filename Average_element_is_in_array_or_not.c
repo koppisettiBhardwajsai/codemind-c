11fd4a8183a45a1b313fe154a72947eb7114e07f
koppisettiBhardwajsai <120609887+koppisettiBhardwajsai@users.noreply.github.com>
2023-07-05 17:18:02 +0530

    #include<stdio.h> int search(int *a,int n,int se) {     int i;     for(i=0;i<n;i++)     {         if(a[i]==se)         {             return i;         }     }     return -1; } int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int se;     for(i=0;i<n;i++)     {         int w=w+a[i];         se=w/n;     }     int s=search(a,n,se);     if(s==-1)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Average_element_is_in_array_or_not.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int search(int *a,int n,int se)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==se)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int se;
+    for(i=0;i<n;i++)
+    {
+        int w=w+a[i];
+        se=w/n;
+    }
+    int s=search(a,n,se);
+    if(s==-1)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
