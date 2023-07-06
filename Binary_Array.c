f45e80fd9cec6a0135698df98ec3ca508c290a35
koppisettiBhardwajsai <120609887+koppisettiBhardwajsai@users.noreply.github.com>
2023-07-06 10:38:38 +0530

    #include<stdio.h> int binary(int *a,int n) {     int i;     for(i=0;i<n;i++)     {         if(a[i]!=0 && a[i]!=1)         {             return -1;         }     }     return 1; } int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {        scanf("%d",&a[i]);     }     int s=binary(a,n);     if(s==-1)     {         printf("False");     }     else     {         printf("True");     } }

--- Binary_Array.c
+++ Binary_Array.c
@@ -4,12 +4,12 @@ int binary(int *a,int n)
     int i;
     for(i=0;i<n;i++)
     {
-        if(a[i]!=1 && a[i]!=0 )
+        if(a[i]!=0 && a[i]!=1)
         {
             return -1;
         }
     }
-    return i;
+    return 1;
 }
 int main()
 {
@@ -18,7 +18,7 @@ int main()
     int a[n],i;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+       scanf("%d",&a[i]);
     }
     int s=binary(a,n);
     if(s==-1)
